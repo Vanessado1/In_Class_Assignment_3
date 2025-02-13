3b6fbfdaad661eb591b46f2754fa3ff52e4eb38e
Vanessa Do <xex.vanessa@gmail.com>
2025-02-13 15:45:54 -0500

    initial commit

---
+++ cc_example.js
@@ -0,0 +1,11 @@
+// Task 5
+let orders = [
+    {id: 101, customer: "Alice", total: 300},
+    {id: 102, customer: "Bob", total: 450},
+    {id: 103, customer: "Charlie", total: 200}
+];
+
+function findOrder(orders, orderId) {
+    return orders.find(order => order.Id === orderId);
+}
+console.log(findOrder(orders,102)); 

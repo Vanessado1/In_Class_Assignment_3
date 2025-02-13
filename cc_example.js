88e453c01c88de76dab348c2bec1a2c98c30074d
Vanessa Do <xex.vanessa@gmail.com>
2025-02-13 16:16:17 -0500

    commiting for everything

--- cc_example.js
+++ cc_example.js
@@ -1,11 +1,53 @@
 // Task 5
-let orders = [
-    {id: 101, customer: "Alice", total: 300},
-    {id: 102, customer: "Bob", total: 450},
-    {id: 103, customer: "Charlie", total: 200}
-];
+// let orders = [
+//     {id: 101, customer: "Alice", total: 300},
+//     {id: 102, customer: "Bob", total: 450},
+//     {id: 103, customer: "Charlie", total: 200},
+// ];
 
-function findOrder(orders, orderId) {
-    return orders.find(order => order.Id === orderId);
+// function findOrders(order, orderId) {
+//     return order.find(orders => order.Id === orderId);
+// };
+// console.log(findOrders(orders,102)); 
+
+// Task 6 
+// let inventory = {
+//     items: [],
+//     addItem(name, quantity) {
+//         this.items.push({name, quantity});
+//     },
+//     removeLastestItem(){
+//         this.items.pop();
+//     },
+//     removeFirstItem(){
+//         this.items.shift();
+//     }
+// };
+// inventory.addItem("Monitro", 5);
+// inventory.addItem("Keyboard", 10);
+// inventory.addItem("Mouse", 3);
+// console.log(JSON.stringify(inventory.items));
+// inventory.removeLastestItem();
+// console.log(JSON.stringify(inventory.items));
+// inventory.removeFirstItem();
+// console.log(JSON.stringify(inventory.items));
+
+// Task 7 
+// let employee = [
+//     {name: "Alice", position: "Developer", salary: 70000},
+//     {name: "Bob", position: "Designer", salary: 60000},
+//     {name: "CHarlie", position: "Manager", salary: 90000}
+// ]
+// function findEmployee(employees, name) {
+//     return employees.find(employee => employee.name === name);
+// };
+// console.log(findEmployee(employee, "Bob")); 
+
+// Task 8 
+let orders1 = [{ id: 1, customer: "Alice"}, {id: 2, cusomter: "Bob"}];
+let orders2 = [{id : 3, customer: "Charlie"}, {id: 4, customer: "David"}];
+
+function mergeOrders(orderA, orderB) {
+    return [...orderA, ...orderB];
 }
-console.log(findOrder(orders,102)); 
+console.log(mergeOrders(orders1, orders2)); 
